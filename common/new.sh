9bc35e7d77387a0b934d3be4e6d3bf380346a7fd
Rodolfo Berrios <20590102+rodber@users.noreply.github.com>
2024-05-07 12:30:32 -0400

    8.2, 8.3

--- common/new.sh
+++ common/new.sh
@@ -128,8 +128,15 @@ EOM
 # scripts/10-php.sh
 cat >/etc/php/8.2/apache2/conf.d/chevereto.ini <<EOM
 log_errors = On
-upload_max_filesize = 64M
-post_max_size = 64M
+upload_max_filesize = 512M
+post_max_size = 512M
+max_execution_time = 60
+memory_limit = 512M
+EOM
+cat >/etc/php/8.3/apache2/conf.d/chevereto.ini <<EOM
+log_errors = On
+upload_max_filesize = 512M
+post_max_size = 512M
 max_execution_time = 60
 memory_limit = 512M
 EOM
