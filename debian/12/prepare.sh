9d30e6aa027483648b40699f56e7574103340c50
Rodolfo Berrios <20590102+rodber@users.noreply.github.com>
2025-05-19 11:21:39 -0400

    debian 12 exiftool exiftran

--- debian/12/prepare.sh
+++ debian/12/prepare.sh
@@ -4,9 +4,9 @@ set -e
 
 if [ "$(id -u)" -ne 0 ]; then
     if ! command -v sudo &>/dev/null; then
-    echo "sudo package is not installed. Please install sudo first."
-    exit 1
-fi
+        echo "sudo package is not installed. Please install sudo first."
+        exit 1
+    fi
     echo "Running with sudo privileges..."
     SUDO='sudo'
 else
@@ -20,12 +20,12 @@ EOF
 
 install_php_repo() {
     if ! command -v curl &>/dev/null; then
-      echo "curl is not installed. Installing curl..."
-      $SUDO apt-get update -qq && $SUDO apt-get install -qq -y curl
+        echo "curl is not installed. Installing curl..."
+        $SUDO apt-get update -qq && $SUDO apt-get install -qq -y curl
     fi
     if ! command -v lsb_release &>/dev/null; then
-      echo "installing lsb_release"
-      $SUDO apt-get update -qq && $SUDO apt-get install -qq -y lsb-release
+        echo "installing lsb_release"
+        $SUDO apt-get update -qq && $SUDO apt-get install -qq -y lsb-release
     fi
     $SUDO curl -sSLo /usr/share/keyrings/deb.sury.org-php.gpg https://packages.sury.org/php/apt.gpg
     $SUDO sh -c 'echo "deb [signed-by=/usr/share/keyrings/deb.sury.org-php.gpg] https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list'
@@ -48,12 +48,12 @@ update() {
 update
 read -p "Do you want to install nginx or apache? (nginx/apache): " webserver_choice
 if [ "$webserver_choice" = "apache" ]; then
-  $SUDO apt-get install -qq -y apache2 libapache2-mod-php8.3 certbot python3-certbot-apache
+    $SUDO apt-get install -qq -y apache2 libapache2-mod-php8.3 certbot python3-certbot-apache
 else
-  $SUDO apt-get install -qq -y nginx-full php8.3-fpm certbot python3-certbot-nginx
+    $SUDO apt-get install -qq -y nginx-full php8.3-fpm certbot python3-certbot-nginx
 fi
 
-$SUDO apt-get install -qq -y ca-certificates software-properties-common unzip ffmpeg cron
+$SUDO apt-get install -qq -y ca-certificates software-properties-common unzip ffmpeg cron libimage-exiftool-perl exiftran
 $SUDO apt-get install -qq -y mariadb-server
 $SUDO apt-get install -qq -y php8.3 php8.3-{bcmath,common,cli,curl,fileinfo,gd,imagick,intl,mbstring,mysql,opcache,pdo,pdo-mysql,xml,xmlrpc,zip}
 
